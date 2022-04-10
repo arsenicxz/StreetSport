ffdad59994edc553b19960c2f082a2d9ca9c169d
arsenicxz <57327489+arsenicxz@users.noreply.github.com>
2022-04-10 20:31:36 +0300

    all testing screens done

--- frontend/street-sport/App.js
+++ frontend/street-sport/App.js
@@ -4,10 +4,11 @@ import { StyleSheet, View } from 'react-native';
 import { Login } from './components/login/Login';
 import { ProfileScreen } from './components/screens/ProfileScreen';
 import { CreateGameScreen } from './components/screens/CreateGameScreen';
+import { SearchScreen } from './components/screens/SearchScreen';
 
 export default function App() {
   return(
-      <CreateGameScreen />
+      <SearchScreen />
   )
 }
 
