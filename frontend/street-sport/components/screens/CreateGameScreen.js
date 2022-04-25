9d5b6e4d935db6b4149bf8aed7bd16c2a6af6719
arsenicxz <57327489+arsenicxz@users.noreply.github.com>
2022-04-25 21:31:46 +0300

    add map in Create Game screen

--- frontend/street-sport/components/screens/CreateGameScreen.js
+++ frontend/street-sport/components/screens/CreateGameScreen.js
@@ -1,4 +1,4 @@
-
+import {useState} from 'react';
 import { 
     StyleSheet, 
     Text, 
@@ -7,10 +7,46 @@ import {
     TextInput, 
     Image, 
     ScrollView,
+    FlatList,
+    TouchableOpacity
 } from 'react-native';
+import {Marker} from 'react-native-maps';
+import MapView from 'react-native-maps';
 import Switcher from '../uiElements/Switcher';
 
 export const CreateGameScreen = props => {
+    const [searchValue, setSearchValue] = useState('');
+    const [searchData, setSearchData] = useState([]);
+    const [latitudeState, setLatitude] = useState(37.425);
+    const [longitudeState, setLongitude] = useState(-122.085);
+
+    const [visibleSearchList, setVisibleSearchList] = useState(false);
+
+    var url = "https://suggestions.dadata.ru/suggestions/api/4_1/rs/suggest/address";
+    var token = "1b276ccfa98080a05a669e7b807851a75a2a0585";
+
+    var options = {
+        method: "POST",
+        mode: "cors",
+        headers: {
+            "Content-Type": "application/json",
+            "Accept": "application/json",
+            "Authorization": "Token " + token
+        },
+        body: JSON.stringify({query: searchValue})
+    };
+
+    async function getResponse(){
+        let response = await fetch(url, options);
+        let content = await response.json();
+        setSearchData(content.suggestions);
+    }
+
+    const searchInputHandler = (enteredtext) =>{
+      setSearchValue(enteredtext);
+      getResponse();
+    };
+
     return (
         <SafeAreaView style={styles.container}>
           {/* <View style={styles.header}>
@@ -49,13 +85,57 @@ export const CreateGameScreen = props => {
                 <TextInput 
                     style={styles.input}
                     placeholder={"Введите адрес"}
+                    onChangeText={searchInputHandler}
+                    value={searchValue}
+                    onPressIn={()=>{
+                      setVisibleSearchList(true);
+                    }}
                 />
+                <SafeAreaView>
+                {
+                    visibleSearchList ? (
+                    <FlatList
+                    style={styles.searchContainer}
+                    data={searchData}
+                    keyExtractor={({id}, index) => id}
+                    renderItem={({item}) => (
+                    <TouchableOpacity style={styles.searchItems} onPress={() => {
+                        if(item.data.geo_lat != null && item.data.geo_lon != null){
+                        setLatitude(parseFloat(item.data.geo_lat));
+                        setLongitude(parseFloat( item.data.geo_lon));
+                        setSearchValue(item.value);
+                        setVisibleSearchList(false);
+                        }       }}>
+                        <Text>{item.value}</Text>
+                    </TouchableOpacity>
+                    )}
+                /> ) : (<View></View>)
+
+                }
+                
+                </SafeAreaView>
               </View>
 
               <View style={styles.contentInfo}>
                 <Text style={styles.contentInfoH2}>на карте</Text>
-                <View>
-
+                <View style={styles.mapInWindow}>
+                  <MapView
+                    style={styles.map} 
+                    // provider="google"
+                    region={{
+                      latitude: latitudeState,
+                      longitude: longitudeState,
+                      latitudeDelta: 0.0922,
+                      longitudeDelta: 0.0481, 
+                    }}
+                  >
+                        <Marker
+                          coordinate={{latitude: latitudeState, longitude: longitudeState}}
+                          title='Here you go!'
+                        >
+                          
+                        </Marker>
+                  </MapView>
                 </View>
               </View>
 
@@ -108,6 +188,31 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         justifyContent: 'center',
         marginVertical: 5,
+      },
+      mapInWindow: {
+        height: 350,
+        borderRadius: 14,
+        borderWidth: 1,
+        borderColor: '#D1D1D1',
+        overflow: 'hidden',
+      },
+      map: {
+        borderRadius: 14,
+        width: "100%",
+        height: "100%",
+      },
+      searchContainer: {
+        maxHeight: 250,
+        backgroundColor: '#F9F9F9',
+        borderRadius: 14,
+        borderWidth: 1,
+        borderColor: '#D1D1D1',
+      },  
+      searchItems: {
+        padding: 16,
+        fontSize: 16,
+        borderBottomWidth:1,
+        borderColor: '#D1D1D1',
       },
   });
 
